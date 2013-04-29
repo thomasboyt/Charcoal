a4f8e792711aa9492cab3b6cfd846e9218705809
Thomas Boyt <me@thomasboyt.com>
2013-04-29 18:04:01 -0400

    fixed incorrect cssmin folder

--- app/templates/charcoal/grunt.js
+++ app/templates/charcoal/grunt.js
@@ -217,8 +217,7 @@ module.exports = {
       dist: {
         files: {
           '<%%= yeoman.dist %>/styles/main.css': [
-            'tmp/styles/{,*/}*.css',
-            '<%%= yeoman.app %>/styles/{,*/}*.css'
+            'assets/styles/{,*/}*.css'
           ]
         }
       }
