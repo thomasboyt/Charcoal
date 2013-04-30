a2a0cc25142ea0ba1679950b0f2daf7b3d224f8d
Thomas Boyt <me@thomasboyt.com>
2013-04-30 16:12:07 -0400

    More elegant Mocha/ember-testing integration

--- app/templates/test/main.js
+++ app/templates/test/main.js
@@ -2,4 +2,8 @@
 <%= namespace %>.setupForTesting();
 <%= namespace %>.injectTestHelpers();
 
+// stub out the start() and stop() qunit methods ember-testing tries to use
+window.start = function() {};
+window.stop = function() {};
+
 require("spec/*");
--- app/templates/test/spec/index_spec.js
+++ app/templates/test/spec/index_spec.js
@@ -8,14 +8,9 @@ describe("Index", function() {
   });
 
   it("contains the words 'Welcome to Ember.js on Charcoal'", function(done) {
-    // ember.js's test promises call start(), which is equivalent to mocha's
-    // done(), so we alias it
-    window.start = done;
-
     visit("/").then(function() {
-      // done() is called after this promise cb executes by ember.js's test
-      // promises
-      expect($("#ember-website h2").html()).to.be.equal("Welcome to Ember.js on Charcoal");
+      expect($("#ember h2").html()).to.be.equal("Welcome to Ember.js on Charcoal");
+      done();
 
       // finishes loading fixtures so Ember Data doesn't throw an error after
       // App.reset() in teardown
